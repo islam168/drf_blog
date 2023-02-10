6264e2b84db72d3b07ecf0eedb56aeb0ea18961d
Islamiddin Islamov <islamov.isl168@gmail.com>
2023-02-10 16:48:27 +0600

    updated blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('posts', PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>', DetailView.as_view(), name='post_detail'),
-    path('categories/', CategoryList.as_view(), name='category_list'),
+    path('categories/', CategoryList.as_view(), name='category_list')
 ]
